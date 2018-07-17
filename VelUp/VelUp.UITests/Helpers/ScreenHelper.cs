166a5abe673fff149503db22262ecf426cd05c59
Anastasia Mizumskaya <ana.mizumskaya@gmail.com>
2018-07-17 21:52:01 +0300

    add  screen helper

---
+++ VelUp/VelUp.UITests/Helpers/ScreenHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Xamarin.UITest;
+
+namespace VelUp.UITests.Helpers
+{
+    public class ScreenHelper
+    {
+        public static void MakeScreen(string screenName, string testsName, IApp app, Platform platform)
+        {
+            var screen = app.Screenshot(screenName);
+
+            string folderPath = @"../../../VelUp/Artifacts/" + app.Device.DeviceIdentifier + "_" + platform.ToString();
+
+            CreateFolderForScreens(folderPath);
+
+            folderPath = folderPath + "/" + testsName;
+
+            CreateFolderForScreens(folderPath);
+
+            var fileName = DateTime.Now.ToString("g") + ".png";
+            fileName = fileName.Replace("/", ":");
+
+            string destinationPath = string.Format(folderPath + "/{0}", screenName + "_" + fileName);
+            screen.MoveTo(destinationPath);
+        }
+
+        #region -- Private helpers --
+
+        private static void CreateFolderForScreens(string folderName)
+        {
+            bool isFolderExist = System.IO.Directory.Exists(folderName);
+
+            if (!isFolderExist)
+            {
+                Directory.CreateDirectory(folderName);
+            }
+        }
+
+        #endregion
+    }
+}
