0123a9085e4988ce7152a32d9525a08dd19540f0
Ana Mi <ana.mizumskaya@gmail.com>
2023-02-28 13:16:49 +0200

    tech: update Nuget packages; up TargetFrameworkVersion for Droid to v12.0

--- VelUp/VelUp.Android/Units/PageController.cs
+++ VelUp/VelUp.Android/Units/PageController.cs
@@ -1,17 +1,18 @@
-﻿using System; 
+﻿using System; using System.Collections.Generic;
 using System.Collections.ObjectModel; 
-using Xamarin.Forms;  
+using Xamarin.Forms; using Xamarin.Forms.Internals;
 
 namespace VelUp.Droid.Units 
 {
-
     public class PageController : IPageController
 
     {
 
         private ReflectedProxy<Page> _proxy;
 
-
+        public event EventHandler<EventArg<VisualElement>> BatchCommitted;
+        public event EventHandler<VisualElement.FocusRequestArgs> FocusChangeRequested;
+        public event EventHandler PlatformSet;
 
         PageController(Page page)
 
@@ -96,6 +97,26 @@ namespace VelUp.Droid.Units 
 
         }
 
+        public bool Batched => throw new NotImplementedException();
+
+        public bool DisableLayout { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public EffectiveFlowDirection EffectiveFlowDirection => throw new NotImplementedException();
+
+        public bool IsInNativeLayout { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsNativeStateConsistent { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsPlatformEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public NavigationProxy NavigationProxy => throw new NotImplementedException();
+
+        public IEffectControlProvider EffectControlProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ReadOnlyCollection<Element> LogicalChildren => throw new NotImplementedException();
+
+        public Element RealParent => throw new NotImplementedException();
+
+        public IPlatform Platform { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
         #endregion 
 
         #region -- IPageController implementation -- 
@@ -127,5 +148,34 @@ namespace VelUp.Droid.Units 
 
         }
 
+        public void NativeSizeChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InvalidateMeasure(InvalidationTrigger trigger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EffectIsAttached(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValueFromRenderer(BindableProperty property, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValueFromRenderer(BindablePropertyKey propertyKey, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Element> Descendants()
+        {
+            throw new NotImplementedException();
+        }
     } 
 }
